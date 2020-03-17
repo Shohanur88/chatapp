e21db828f3bb41bcbee0bce2b0c387fd4f78d5cc
Shohanur88 <43010815+Shohanur88@users.noreply.github.com>
2020-03-16 18:03:40 -0700

    make it better

--- router.js
+++ router.js
@@ -8,42 +8,42 @@ const sql = require('./Sql');
 
 let sessio ;
 
-router.get('/chat', (req, res) =>{
-    res.sendFile(__dirname + '/index.html');
+router.get('/', (req, res) =>{
+    res.sendFile(__dirname + 'client/build/index.html');
 });
 
-router.get('/',(req,res)=>{
+router.get('/ser',(req,res)=>{
     res.send('server is up and running');
 
 });
 
-router.post("/loginroom", (req, res) => {
-   sessio = req.session;
+// router.post("/loginroom", (req, res) => {
+//    sessio = req.session;
    
-   sql.SqlConnect();
-   const userData={
-       username: req.body.username,
-       password: req.body.password
-   };
-   sql.SqlLoginUser(userData,(check,result)=>{
-       console.log(check);
-       console.log(result);
-       if(check === true){
-            // session.username = result.username;
-            // session.role = result.role;
-            // session.uid = result.uid;
-            // session.fname = result.fname;
-       }
-       res.send(result);
-   })
-//    sql.SqlLoginUser(userData,(check)=>{
+//    sql.SqlConnect();
+//    const userData={
+//        username: req.body.username,
+//        password: req.body.password
+//    };
+//    sql.SqlLoginUser(userData,(check,result)=>{
 //        console.log(check);
-//    });
-//    //console.log(userData);
-   sql.SqlDisconnect();
-   //res.send({Msg:"Its login page"});
-   //console.log("rq accepted");
-});
+//        console.log(result);
+//        if(check === true){
+//             // session.username = result.username;
+//             // session.role = result.role;
+//             // session.uid = result.uid;
+//             // session.fname = result.fname;
+//        }
+//        res.send(result);
+//    })
+// //    sql.SqlLoginUser(userData,(check)=>{
+// //        console.log(check);
+// //    });
+// //    //console.log(userData);
+//    sql.SqlDisconnect();
+//    //res.send({Msg:"Its login page"});
+//    //console.log("rq accepted");
+// });
 
 router.post("/registerblock", (req, res) => {
     //sessio = req.session;
