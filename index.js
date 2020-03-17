802543dee2f00003bbcec798fa5fe726eadb3afe
Shohanur88 <43010815+Shohanur88@users.noreply.github.com>
2020-03-16 17:54:25 -0700

    make it better

--- index.js
+++ index.js
@@ -383,8 +383,7 @@ app.use(express.json());
 
 //if(process.env.NODE_ENV === 'devlopment'){
   app.use(express.static('client/build'));
-  app.get('/',(req,res)=>{
-    console.log("Hello");
+  app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'client','build','index.html'));
   })
 //}
